496f4b47a8bf6424d6ed7d0978fec57e6e3b156e
Rachel Anupama <rachelanupama19@gmail.com>
2026-03-03 20:46:58 +0530

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,81 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#variables
+print (3+4)
+7
+u=9
+print(u)
+9
+a=3
+b=4
+print(a+b)
+7
+a7=10
+print(a7)
+10
+r=3;o=9
+print (r+o)
+12
+c,d = 9,10
+print(c+d)
+19
+email = "rachelanupama19@gmail.com"
+print(email)
+rachelanupama19@gmail.com
+fname = "Rachel"
+lname = "Anupama"
+print (fname +" "+ lname)
+Rachel Anupama
+print (fname,lname)
+Rachel Anupama
+A= 100
+print (A)
+100
+>>> print a1234 = "India"
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?
+>>> a12345 = "india'
+SyntaxError: unterminated string literal (detected at line 1)
+>>> a12345 = "india"
+>>> print(a12345)
+india
+>>> data= " rachel @*$#&* "
+>>> print(data)
+ rachel @*$#&* 
+>>> first_name = "Rachel"
+>>> last_name = "Anupama"
+>>> print (first_name+" "+last_name)
+Rachel Anupama
+>>> print (first_name,last_name)
+Rachel Anupama
+>>> 
+>>> #del
+>>> del u
+>>> print(u)
+Traceback (most recent call last):
+  File "<pyshell#34>", line 1, in <module>
+    print(u)
+NameError: name 'u' is not defined
+>>> u=99
+>>> print(u)
+99
+>>> 
+>>> #python is a case sensitive language
+>>> Name = "rachel"
+>>> print (Name)
+rachel
+>>> NAME = "Anupama"
+>>> print(NAME)
+Anupama
+>>> name = "rachel"
+>>> print(name)
+rachel
+>>> 
+>>> #dont use keywords as variables
+>>> print = 3
+>>> print(print)
+Traceback (most recent call last):
+  File "<pyshell#48>", line 1, in <module>
+    print(print)
+TypeError: 'int' object is not callable
+>>> if = 3
+SyntaxError: invalid syntax
---
+++ run time input.py
@@ -0,0 +1,22 @@
+# run time input ()
+
+'''a = int(input("Enter the value of a"))
+b = int(input("Enter the value of b"))
+print(a+b)'''
+
+a = float(input("Enter the value of a"))
+b =  float(input("Enter the value of b"))
+print(a+b)
+
+a = complex(input("Enter the value of a"))
+b =  complex(input("Enter the value of b"))
+print(a+b)
+
+a = str(input("Enter the value of a"))
+b =  str(input("Enter the value of b"))
+print(a+b)
+
+
+
+
+
