d0077f29369fea47ea0f73ab641bd3d42eb700a4
Rachel Anupama <rachelanupama19@gmail.com>
2026-03-05 17:13:03 +0530

    Add files via upload

---
+++ Fee Calculator.py
@@ -0,0 +1,9 @@
+#fee calculator
+
+hostel_fee = int(input("enter hostel fee:"))
+course_fee = int(input("enter course fee:"))
+total = hostel_fee + course_fee
+print("Fees...............")
+print("Hostel Fee :",hostel_fee)
+print("Course Fee;",course_fee)
+print( "Total fee -------->", total)
---
+++ if with condition.py
@@ -0,0 +1,53 @@
+'''a = 3
+b = 4
+if a>b:
+    print ("true")'''
+
+'''a = 3
+b = 4
+if a<b:
+    print ("true")'''
+
+'''a = 3
+b = 4
+if a<=b:
+    print ("true")    '''
+
+'''a = 3
+b = 4
+if a>=b:
+    print ("true")'''
+
+'''a = 4
+b = 3
+if a>=b:
+    print ("true")'''
+
+'''a = 3
+b = 4
+if a != b:
+    print ("not equal")'''
+
+'''a = 3
+b = 3
+if a==b:
+    print ("equal") '''
+
+#run_time
+
+'''a = int(input("enter a"))
+b = int(input("enter b"))
+if a<b:
+    print ("less")'''
+
+'''a = int(input("enter a"))
+if a>10:
+    print("true")'''
+
+'''a = python
+if a != "java":
+    print(" not equal")'''
+
+a = input("data")
+if a =="python":
+    print("true")
