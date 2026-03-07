2109c5de05c02798b29131b7335b7b41aa055f14
Rachel Anupama <rachelanupama19@gmail.com>
2026-03-07 16:50:50 +0530

    Add files via upload

---
+++ nested if.py
@@ -0,0 +1,80 @@
+# nested if
+
+'''a = 15
+b = 30
+if a<b:
+    print("less")
+    if b>a:
+        print("greater")'''
+
+'''a = 15
+b = 30
+if a<b:
+    print("less")
+if b>a:
+        print("greater")'''
+
+'''a = 15
+b = 30
+if a<b:
+    print("less")
+    if b>a:
+         print("greater")'''
+
+'''a = 15
+b = 30
+if a<b:
+    print("less")
+    if b==a:
+        print("greater")'''
+
+'''a = 15
+b = 30
+if a<b:
+    print("less")
+    if b==a:
+        print("greater")
+    else:
+        print("true")'''
+
+'''a = 15
+b = 30
+if a>b:
+    print("less")
+    if b>a:
+        print("greater")
+else:
+    print("true")'''
+
+'''a = 15
+b = 30
+if a>b:
+    print("less")
+    if b>a:
+        print("greater")
+        else:
+            print("false")
+else:
+    print("true")'''
+
+a = 15
+b = 30
+if a<b:
+    print("less")
+    if b>a:
+        print("greater")
+    elif a!= b:
+        print("not equal")
+    else:
+        print("false")
+else:
+    print("true")
+
+
+
+
+
+
+        
+
+
---
+++ real life usecases with if.py
@@ -0,0 +1,84 @@
+#voting
+
+'''age = int(input("enter the age"))
+if age >= 18:
+          print("eligible to vote")
+else:
+    print("not eligible to vote")'''
+
+#even or odd
+
+'''n = int(input("enter a value"))
+if n%2 == 0:
+    print("it is even")
+else:
+    print("it is odd")'''
+
+#vowels
+
+'''n = input("enter a value").lower()
+vow = ['a', 'e','i','o','u']
+if n in vow:
+    print ("it is a vowel")
+else:
+    print("consonant")'''
+
+#leap year
+
+'''year = int(input('enter the year'))
+if year%4 == 0:
+    print( "leap year")
+else:
+    print("not a leap year")'''
+
+#guest code
+
+'''name = input('enter a name')
+if name == 'rachel':
+    print("welcome Rachel!")
+else:
+    print("welcome guest!")'''
+
+#multiple users
+
+'''n = input('enter a name').lower()
+name=['rachel', 'anupama','bird']
+if n in name:
+    print("welcome",n)
+else:
+    print("welcome guest!")'''
+
+#social media password
+
+'''username = input("enter username")
+password = input('enter the password')
+if username == 'rachel':
+    if passoword == '123':
+        print("login suscessful")
+    else:
+        print("invalid passoword")
+else:
+    print("invalid username")'''
+
+'''username = input("enter username")
+password = input('enter the password')
+if username == 'rachel' and passoword == '123':
+    print("login suscessful")
+else:
+    print("invalid credintials")'''
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
