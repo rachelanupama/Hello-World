02f4b608ee32bed5de2dfb1215a8501667034492
Rachel Anupama <rachelanupama19@gmail.com>
2026-03-15 22:53:12 +0530

    Add files via upload

---
+++ data structures and loops.py
@@ -0,0 +1,17 @@
+'''numbers = [10,20,30,65,89]
+odd = []
+for i in numbers:
+    if i % 2 != 0:
+        odd.append(i)
+print (odd)'''
+
+details = {"maths": 80, "science":44,"social":22,"history":13}
+less_than_40 = {}
+for key in details:
+    if details[key] < 40:
+        less_than_40[key] = details [key]
+print (less_than_40)
+
+
+
+
