21d60d4d461dc408c4a65d65eec3aeb8b9bf9e7c
Rachel Anupama <rachelanupama19@gmail.com>
2026-03-20 19:38:33 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,80 @@
+#attendance report
+'''s = int(input("enter num of students: "))
+pr = 0
+ab  = 0
+for i in range(s):
+    c = input(("enter a / p ")).lower()
+    if c == 'p':
+        pr+=1
+    else:
+        ab+=1
+print(f"total students: {s}")
+print(f"no of students present:{pr}")
+print(f"no of students absent : {ab}")'''
+
+#course fee
+'''fee = int(input("enter the course fee: "))
+hostel_fee = int("enetr the hostel fee: ")
+print("COURSE FEE.........................")
+print("Course Fee : {fee}")
+print("Hostel Fee : {hostel_fee}")
+print("TOTAL : {fee + hostel_fee}")'''
+
+#multiplication table
+n = int(input("enter a number: "))
+for i in range(21):
+    print(f"{n} x {i} = {n*i}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
